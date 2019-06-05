fe3e8ac542d0f66235c8911a4a61a074eeba3b4a
Emily Lam <elam@bomgar.com>
2019-06-05 00:26:27 -0700

    add blockstack

--- public/app.js
+++ public/app.js
@@ -1,36 +1,10 @@
-import defaultExport from "blockstack";
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom';
 
-document.addEventListener("DOMContentLoaded", event => {
-  const appConfig = new blockstack.AppConfig()
-  const userSession = new blockstack.UserSession({ appConfig: appConfig })
+import App from './components/App.jsx';
 
-  document.getElementById('signin-button').addEventListener('click', event => {
-    event.preventDefault()
-    userSession.redirectToSignIn()
-  })
+// Require Sass file so webpack can build it
+import bootstrap from 'bootstrap/dist/css/bootstrap.css';
+import style from './styles/style.css';
 
-  document.getElementById('signout-button').addEventListener('click', event => {
-    event.preventDefault()
-    userSession.signUserOut()
-    window.location = window.location.origin
-  })
-
-  function showProfile (profile) {
-    let person = new blockstack.Person(profile)
-    document.getElementById('heading-name').innerHTML = person.name() ? person.name() : "Nameless Person"
-    if(person.avatarUrl()) {
-      document.getElementById('avatar-image').setAttribute('src', person.avatarUrl())
-    }
-    document.getElementById('section-1').style.display = 'none'
-    document.getElementById('section-2').style.display = 'block'
-  }
-
-  if (userSession.isUserSignedIn()) {
-    const { profile } = userSession.loadUserData()
-    showProfile(profile)
-  } else if (userSession.isSignInPending()) {
-    userSession.handlePendingSignIn().then(userData => {
-      window.location = window.location.origin
-    })
-  }
-})
+ReactDOM.render(<App />, document.getElementById('root'));
