8dda0f21e1a23acc936a84950964695db35af93f
Emily Lam <elam@bomgar.com>
2019-06-04 23:54:51 -0700

    hide script

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ function allowCrossDomain (req, res, next) {
   next()
 }
 
+app.use(function(req, res, next) {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
+})
+
 app.use(allowCrossDomain)
 app.use('/', express.static(`${__dirname}/public`))
 
