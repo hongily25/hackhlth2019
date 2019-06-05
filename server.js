081b76d88858d6bff779b7c8b2a19830148b4a26
Emily Lam <elam@bomgar.com>
2019-06-05 00:40:14 -0700

    mime

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(function(req, res, next) {
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Pragma', 'no-cache');
   res.setHeader('Expires', '0');
+  res.setContentType("text/html");
   next();
 })
 
