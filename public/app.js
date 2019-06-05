697b6710bd181c1ad06c0f84dbb0a12603de2c9f
Emily Lam <elam@bomgar.com>
2019-06-05 00:35:37 -0700

    add blockstack

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-import * as blockstack from 'blockstack'
+import * as blockstack from '../node_modules/blockstack'
 
 document.addEventListener("DOMContentLoaded", event => {
   const appConfig = new blockstack.AppConfig()
