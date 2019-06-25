6f030d95d8ec7c21934bbb550644e99ec508ed46
Emily Lam <elam@bomgar.com>
2019-06-24 22:32:00 -0700

    rev 73

--- public/requires.js
+++ public/requires.js
@@ -83672,43 +83672,6 @@ function getZoneFileTemplate() {
 
 window.blockstack = require('blockstack');
 
-document.addEventListener("DOMContentLoaded", event => {
-  const appConfig = new blockstack.AppConfig()
-  const userSession = new blockstack.UserSession({ appConfig: appConfig })
-
-  document.getElementById('signin-button').addEventListener('click', event => {
-    event.preventDefault()
-    userSession.redirectToSignIn()
-  })
-
-  document.getElementById('signout-button').addEventListener('click', event => {
-    event.preventDefault()
-    userSession.signUserOut()
-    window.location = window.location.origin
-  })
-
-  function showProfile (profile) {
-    let person = new blockstack.Person(profile);
-    console.log('person', person);
-    document.getElementById('heading-name').innerHTML = person.name() ? person.name() : "world"
-    if(person.avatarUrl()) {
-      document.getElementById('avatar-image').setAttribute('src', person.avatarUrl())
-    }
-    document.getElementById('section-1').style.display = 'none'
-    document.getElementById('section-2').style.display = 'block'
-  }
-
-  if (userSession.isUserSignedIn()) {
-    const profile = userSession.loadUserData().profile;
-    console.log('profile test3: ', profile);
-    showProfile(profile)
-  } else if (userSession.isSignInPending()) {
-    userSession.handlePendingSignIn().then(userData => {
-      window.location = window.location.origin
-    })
-  }
-})
-
 
 
 },{"blockstack":98}],403:[function(require,module,exports){
