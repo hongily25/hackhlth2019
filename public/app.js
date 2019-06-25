27ff9e2627a2108a96abb1f89f43e7f1b498bff4
Emily Lam <elam@bomgar.com>
2019-06-24 20:38:54 -0700

    rev 47

--- public/app.js
+++ public/app.js
@@ -60,13 +60,10 @@ document.addEventListener("DOMContentLoaded", event => {
 
         const category = document.getElementById('expense-category').value;
         const expenseAmount = parseInt(document.getElementById('expense-amount').value);
-        const expense = {
-            expenses: expenses.push(
-              { 
+        const expense = { 
                 category: category,
                 expenseAmount: expenseAmount,
-              })
-        };
+              };
         console.log('expense to be saved', expense);
         // console.log('prev expenses', prevExpenses);
         // console.log('category input', category);
