2520a6cb40620eda8ea94b3d2dec2e4c470317af
Emily Lam <elam@bomgar.com>
2019-06-24 23:06:12 -0700

    rev 81

--- public/app.js
+++ public/app.js
@@ -97,12 +97,9 @@ document.addEventListener("DOMContentLoaded", event => {
     userSession.getFile("/expense5.json", { decrypt: false })
     .then((fileContents) => {
       var expenses = JSON.parse(fileContents || '[]');
-      
-      if (expenses.length === 1) { 
+      if (expenses.length === 0) { 
         document.getElementById('expenses').style.display = 'none';
         document.getElementById('expense-body').innerHTML = '';
-      } else if (expenses.length === 0) {
-        return;
       } else {
         expenses.pop(); 
         console.log('after deleting last item', expenses);
@@ -112,7 +109,6 @@ document.addEventListener("DOMContentLoaded", event => {
         });
         document.getElementById('expense-body').innerHTML = rows;
         userSession.putFile("/expense5.json", JSON.stringify(expenses), { decrypt: false });  
-
       }; 
     })
   }
